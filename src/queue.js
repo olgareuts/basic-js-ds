5014715d05da5bd3d96fde3afeea28e6367073c4
olgareuts <olga.re@mail.ru>
2023-04-17 11:22:01 +0300

    fix: return deleted task file

---
+++ src/queue.js
@@ -0,0 +1,46 @@
+const { NotImplementedError } = require('../extensions/index.js');
+
+const { ListNode } = require('../extensions/list-node.js');
+
+/**
+ * Implement the Queue with a given interface via linked list (use ListNode extension above).
+ *
+ * @example
+ * const queue = new Queue();
+ *
+ * queue.enqueue(1); // adds the element to the queue
+ * queue.enqueue(3); // adds the element to the queue
+ * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
+ * queue.getUnderlyingList() // returns { value: 3, next: null }
+ */
+class Queue {
+  constructor() {
+    this.head = null;
+  }
+
+  getUnderlyingList() {
+    return this.head;
+  }
+
+  enqueue(value) {
+    if (!this.head) {
+      this.head = new ListNode(value);
+    } else {
+      let tail = this.head;
+      while(tail.next) {
+        tail = tail.next;
+      }
+      tail.next = new ListNode(value);
+    }
+  }
+
+  dequeue() {
+    let headValue = this.head.value;
+    this.head = this.head.next;
+    return headValue;
+  }
+}
+
+module.exports = {
+  Queue
+};
